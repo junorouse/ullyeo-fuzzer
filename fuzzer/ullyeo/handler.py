8f577b8dc11f8e05153eba9947e29f7a9b13064f
junorouse <junorouse@gmail.com>
2017-02-19 15:12:13 +0900

    Add parse case sensitive.

--- fuzzer/ullyeo/handler.py
+++ fuzzer/ullyeo/handler.py
@@ -6,8 +6,35 @@ from .parser import BaseParser
 class BaseHandler(WebSocket):
     def handleMessage(self):
         request = BaseParser(self.data)
-        pprint (request.detail['url'])
 
+        type = request.type
+        if type == 'Request':
+            # do request
+            try:
+                request_body = request.detail['requestBody']
+                print (request.type),
+                print (request.detail['method']),
+                pprint (request_body)
+            except KeyError as e:
+                pass
+            pass
+        elif type == 'SendHeaders':
+            # do send headers
+            request_headers = request.detail['requestHeaders']
+            pass
+        elif type == 'Received':
+            # do received
+            response_headers = request.detail['responseHeaders']
+            pass
+        elif type == 'Body':
+            # do body
+            pass
+        elif type == 'Completed':
+            # do complted
+            response_headers = request.detail['responseHeaders']
+            pass
+
+        
     def handleConnected(self):
        print (self.address, 'connected')
 
--- fuzzer/ullyeo/parser.py
+++ fuzzer/ullyeo/parser.py
@@ -1,12 +1,12 @@
 from json import loads
 
 class BaseParser(object):
-    
     def __init__(self, detail: str=""):
         """init parser
 
         Keyword arguments:
         detail -- chrome request's detail parrt (default "")
         """
-        self.status = 0
-        self.detail = loads(detail)['Details']
+        self.tmp = loads(detail)
+        self.detail = self.tmp['Details']
+        self.type = self.tmp['Type']
